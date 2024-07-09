681acc4dce4e25ecd25d83c9fb0d0596469b84fa
luig04 <luischilito@itipopayan.edu.co>
2024-07-08 16:55:27 -1200

    se agrega

---
+++ database/migrations/2024_07_08_143908_create_reservas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('habitacion_id')->constrained('habitaciones')->onDelete('cascade');
+            $table->foreignId('agencia_id')->constrained('agencias')->onDelete('cascade');
+            $table->foreignId('particular_id')->constrained('particular')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservas');
+    }
+};
+
